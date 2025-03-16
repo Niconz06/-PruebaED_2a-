febcf77448b4f911f3da87586a4a8b63d40592e3
Nico <nicolaszabalamartinez@gmail.com>
2025-03-16 21:28:58 +0100

    implementacion test aniadirAlumno

--- Curso.java
+++ Curso.java
@@ -4,10 +4,38 @@
  */
 package instituto;
 
+import java.util.TreeSet;
+
 /**
  *
- * @author Nico
+ * @author ProfDiurno
  */
 public class Curso {
-    
+
+    private String nombre;
+    private TreeSet<Persona> listaAlumnos;
+
+    protected String getNombre() {
+        return nombre;
+    }
+
+    public Curso(String nombre) {
+        this.nombre = nombre;
+        listaAlumnos = new TreeSet<>();
+    }
+
+    @Override
+    public String toString() {
+        String s = "--------------------" + nombre + "-----------------\n";
+        s += "NumExp\tNIF\t\tNombre\t\tApellidos\n";
+        s += "-------------------------------------------------\n";
+        for (Persona listaAlumno : listaAlumnos) {
+            s += listaAlumno + "\n";
+        }
+        return s;
+    }
+
+    public void aniadirAlumno(Persona p) {
+        listaAlumnos.add(p);
+    }
 }
